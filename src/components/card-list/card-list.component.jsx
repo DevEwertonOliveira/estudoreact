3f59a0c7426c507451e4f7c8140592224d8131b1
DevEwertonOliveira <ewerton2007@gmail.com>
2023-03-09 11:20:35 -0400

    app pronto monster rolodex 1.0

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,28 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
-    render(){
+  render() {
+    const { monsters } = this.props;
 
-        const { monsters } = this.props
-       
-        return (
-            <div className="Card-list">
-             {monsters.map(monster => (
-                <h1 key={monster.id}>{monster.name}</h1>
-             ))}
-            </div>
-        )
-    }
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+          <div className="card-container" key={monster.id}>
+            <img
+              alt={`monster ${name}`}
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+          </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
-export default CardList
+export default CardList;
