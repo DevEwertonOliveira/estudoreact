836c25467819e67af6856a4be2dc6ee5fba8f822
DevEwertonOliveira <ewerton2007@gmail.com>
2023-03-10 07:40:26 -0400

    projeto finalizado monster rolodex

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,24 @@
 import { Component } from "react";
-import Card from "../card/card.component"
+import Card from "../card/card.component";
 import "./card-list.styles.css";
 
 class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
+  render(){
+    const {monsters} = this.props
 
     return (
       <div className="card-list">
-        {monsters.map((monster) => {
-          monster;
+        {monsters.map((monster) =>{
+          return (
+            <Card monster={monster} />
+          )
         })}
-        return <Card monster={monster} />
+
+
+
+
       </div>
-    );
+    )
   }
 }
 
